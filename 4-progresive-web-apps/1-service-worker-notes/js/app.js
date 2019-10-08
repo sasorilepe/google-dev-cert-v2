f9d925a5b179c1eef07fcafe92a4267cb55406ca
Alfredo Alvarado Lepe <alfredo.alvarado@ksquareinc.com>
2019-10-08 09:18:12 -0500

    service workers notes added

--- 4-progresive-web-apps/1-service-worker-notes/js/app.js
+++ 4-progresive-web-apps/1-service-worker-notes/js/app.js
@@ -1,1 +1,11 @@
-
+window.addEventListener('load', () => {
+  navigator.serviceWorker.register('service-worker.js',{
+    scope: '/example'
+  })
+  .then(registration => {
+    console.log('SW registered with scope:', registration.scope);
+  })
+  .catch(err => {
+    console.error('Registration failed:', err);
+  });
+});
--- 4-progresive-web-apps/1-service-worker-notes/service-worker.js
+++ 4-progresive-web-apps/1-service-worker-notes/service-worker.js
@@ -1,3 +1,12 @@
 self.addEventListener('install', event => {
+  console.log('Installing');
   self.skipWaiting();
+});
+
+self.addEventListener('activate', event =>{
+  console.log('Activating');
+});
+
+self.addEventListener('fetch', event =>{
+  console.log('Fetching', event.request.url);
 });
